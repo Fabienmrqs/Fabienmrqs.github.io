c70a9de6db1c13781fad925084164d5325a54041
Marques Fabien <fabienmrqs@gmail.com>
2023-01-10 23:53:03 +0100

    add carousel wip

---
+++ carousel.js
@@ -0,0 +1,28 @@
+const next = document.querySelector('#next');
+const back = document.querySelector('#back');
+const slider = document.querySelector('#slider');
+
+var sectionIndex = 0;
+
+function reset() {
+    for ( var i = 0;i < slider.children.length; i++){
+        slider.children[i].style.opacity = 0;
+        slider.children[i].style.zIndex = 0;
+    }
+}
+
+next.addEventListener('click', function(){
+    reset();
+    sectionIndex = sectionIndex < 2 ? sectionIndex + 1 : 2;
+    slider.children[sectionIndex].style.zIndex = 1;
+
+    slider.children[sectionIndex].style.opacity = 1;
+});
+
+back.addEventListener('click', function(){
+    reset();
+    sectionIndex = sectionIndex > 0 ? sectionIndex - 1 : 0;
+    slider.children[sectionIndex].style.zIndex = 1;
+
+    slider.children[sectionIndex].style.opacity = 1;
+});
